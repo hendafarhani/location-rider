41f2465d568529fc5703689e86876fa4fe3f7826
hendafarhani <farhanii.henda@gmail.com>
2026-03-16 06:31:48 +0100

    refactor: change main method visibility to public in LocationRiderApplication

--- src/main/java/com/tracker/location_rider/LocationRiderApplication.java
+++ src/main/java/com/tracker/location_rider/LocationRiderApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class LocationRiderApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(LocationRiderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LocationRiderApplication.class, args);
+    }
 
 }
